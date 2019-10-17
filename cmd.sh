0776135eed857e8f09b8ba0bfc07b5bb560ade92
Chris Dostert <chris.dostert@live.com>
2019-10-17 16:41:35 -0700

    add digest

--- cmd.sh
+++ cmd.sh
@@ -9,12 +9,14 @@ envsubst < /root/.docker/config.template.json > /root/.docker/config.json
 # ensure valid cache layout exists
 cp -R /default_cache/. /cacheDir
 
+# until buildkit uses latest tag by default; need to provide explicit cache tag
+digest=$(cat /cacheDir/index.json | jp -u "manifests[?annotations.\"org.opencontainers.image.ref.name\"=='latest'].digest | [0]" )
+
 buildctl-daemonless.sh \
   build \
   --frontend dockerfile.v0 \
   --local context=/buildContext \
   --local dockerfile=/ \
   --output type=oci,dest=/image.tar \
-  --import-cache type=local,src=/cacheDir \
+  --import-cache "type=local,src=/cacheDir,digest=$digest" \
   --export-cache type=local,dest=/cacheDir
-
