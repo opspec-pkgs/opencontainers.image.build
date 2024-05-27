23fa1cc4c9c6709576295570e8568241a1354406
Chris Dostert <=>
2024-05-27 01:05:44 -0700

    Avoid unnecessary intermediate tar/untar of OCI Image Layout

--- cmd.sh
+++ cmd.sh
@@ -9,10 +9,6 @@ buildctl-daemonless.sh \
   --frontend dockerfile.v0 \
   --local context=/buildContext \
   --local dockerfile=/ \
-  --output type=oci,dest=/image.tar \
+  --output type=oci,tar=false,dest=/image \
   --import-cache type=local,src=/cacheDir \
-  --export-cache type=local,dest=/cacheDir
-
-# @todo remove once https://github.com/moby/buildkit/issues/1219
-echo "untarring image"
-tar -xf /image.tar -C /image
+  --export-cache type=local,dest=/cacheDir,mode=max
